f1f83dcc135b416c038022143c33ff6a3954ed4e
padelclilecito-gestion <padelchilecito@gmail.com>
2025-10-01 01:48:28 -0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,9 @@
 import { Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import ShopPage from './pages/ShopPage';
-// import AdminLayout from './pages/admin/AdminLayout';
-// import LoginPage from './pages/LoginPage';
+import AdminLayout from './pages/admin/AdminLayout';
+import LoginPage from './pages/LoginPage';
+import ProtectedRoute from './components/auth/ProtectedRoute';
 
 function App() {
   return (
@@ -10,10 +11,17 @@ function App() {
       {/* Public Routes */}
       <Route path="/" element={<HomePage />} />
       <Route path="/shop" element={<ShopPage />} />
-      {/* <Route path="/login" element={<LoginPage />} /> */}
+      <Route path="/login" element={<LoginPage />} />
 
       {/* Admin Routes */}
-      {/* <Route path="/admin/*" element={<AdminLayout />} /> */}
+      <Route 
+        path="/admin/*" 
+        element={
+          <ProtectedRoute>
+            <AdminLayout />
+          </ProtectedRoute>
+        } 
+      />
     </Routes>
   );
 }
