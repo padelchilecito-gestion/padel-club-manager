092285809a98afbf7c62b95eb9ca0a466a4d9acb
padelclilecito-gestion <padelchilecito@gmail.com>
2025-10-01 02:31:05 -0300

    Update bookings.js

--- server/routes/bookings.js
+++ server/routes/bookings.js
@@ -8,7 +8,7 @@ const {
   cancelBooking,
   getBookingAvailability,
 } = require('../controllers/bookingController');
-const { protect, admin } = require('../middlewares/authMiddleware');
+const { protect } = require('../middlewares/authMiddleware');
 
 // @route   POST api/bookings
 // @desc    Create a new booking
@@ -19,6 +19,7 @@ router.post('/', createBooking);
 // @desc    Get availability for a specific date
 // @access  Public
 router.get('/availability', getBookingAvailability);
+console.log('[Routes] Booking route /availability registered.');
 
 // @route   GET api/bookings
 // @desc    Get all bookings
