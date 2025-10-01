8bdfa0bb10bcce0d25ab60250902c27f9aa218ae
padelclilecito-gestion <padelchilecito@gmail.com>
2025-10-01 02:20:14 -0300

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -3,14 +3,13 @@ require('dotenv').config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log('MongoDB Connected...');
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (err) {
-    console.error(err.message);
-    // Exit process with failure
+    console.error(`Error: ${err.message}`);
     process.exit(1);
   }
 };
