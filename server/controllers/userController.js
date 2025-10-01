4e7e2d1ffe8fa463925a7d31950d50859a68959f
padelclilecito-gestion <padelchilecito@gmail.com>
2025-10-01 02:27:39 -0300

    Update userController.js

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,16 +1,9 @@
 const User = require('../models/User');
-const jwt = require('jsonwebtoken');
-require('dotenv').config();
-
-const generateToken = (id, role, username) => {
-  return jwt.sign({ id, role, username }, process.env.JWT_SECRET, {
-    expiresIn: '30d',
-  });
-};
+const { logActivity } = require('../utils/logActivity');
 
 // @desc    Register a new user
 // @route   POST /api/users/register
-// @access  Public
+// @access  Admin
 const registerUser = async (req, res) => {
   const { username, password, role } = req.body;
 
@@ -28,11 +21,13 @@ const registerUser = async (req, res) => {
     });
 
     if (user) {
+      const logDetails = `User '${user.username}' with role '${user.role}' was created by admin '${req.user.username}'.`;
+      await logActivity(req.user, 'USER_REGISTERED', logDetails);
+
       res.status(201).json({
         _id: user._id,
         username: user.username,
         role: user.role,
-        token: generateToken(user._id, user.role, user.username),
       });
     } else {
       res.status(400).json({ message: 'Invalid user data' });
@@ -43,6 +38,49 @@ const registerUser = async (req, res) => {
   }
 };
 
+// @desc    Get all users
+// @route   GET /api/users
+// @access  Admin
+const getAllUsers = async (req, res) => {
+    try {
+        const users = await User.find({}).select('-password');
+        res.json(users);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Server Error' });
+    }
+};
+
+// @desc    Delete a user
+// @route   DELETE /api/users/:id
+// @access  Admin
+const deleteUser = async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+
+        if (user) {
+            if (user._id.equals(req.user.id)) {
+                return res.status(400).json({ message: 'Cannot delete the currently logged-in admin.' });
+            }
+            const deletedUsername = user.username;
+            await user.remove();
+            
+            const logDetails = `User '${deletedUsername}' was deleted by admin '${req.user.username}'.`;
+            await logActivity(req.user, 'USER_DELETED', logDetails);
+
+            res.json({ message: 'User removed' });
+        } else {
+            res.status(404).json({ message: 'User not found' });
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Server Error' });
+    }
+};
+
+
 module.exports = {
   registerUser,
+  getAllUsers,
+  deleteUser,
 };
