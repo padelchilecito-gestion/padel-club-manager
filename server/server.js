83a147d9ac14a56a436e54d51a9d74e925aaf1a3
padelclilecito-gestion <padelchilecito@gmail.com>
2025-10-13 11:41:56 -0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -62,9 +62,9 @@ const startServer = async () => {
 
   const PORT = process.env.PORT || 5000;
 
-  server.listen(PORT, '0.0.0.0', () => {
-    console.log(`Server started on port ${PORT}`);
-  });
+  server.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
 };
 
 startServer();
