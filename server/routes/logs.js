87c842e5bd0f88b45a19e964a013d0b8aaaeb825
padelclilecito-gestion <padelchilecito@gmail.com>
2025-11-03 14:06:29 -0300

    Update logs.js

--- server/routes/logs.js
+++ server/routes/logs.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const router = express.Router();
 const { getLogs } = require('../controllers/logController');
-const { protect, admin } = require('../middlewares/authMiddleware'); // <-- CORREGIDO
+const { protect, admin } = require('../middlewares/authMiddleware');
 
 // Solo Admin puede ver los logs
 router.get('/', protect, admin, getLogs); // <-- CORREGIDO
