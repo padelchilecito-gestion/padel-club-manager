4efeb44f8418f9b36895b12942d76bd80bbed7dc
padelclilecito-gestion <padelchilecito@gmail.com>
2025-11-03 16:02:13 -0300

    Update cashbox.js

--- server/routes/cashbox.js
+++ server/routes/cashbox.js
@@ -2,28 +2,36 @@
 const express = require('express');
 const router = express.Router();
 const {
-  // --- CORRECCIÓN DE NOMBRES ---
-  startCashboxSession,
-  endCashboxSession,
-  getActiveCashboxSession,
-  addCashboxMovement,
-  getCashboxSummary,
+  startSession,
+  endSession,
+  getSession,
+  addMovement,
+  getSummary,
 } = require('../controllers/cashboxController');
 const { protect, adminOrOperator } = require('../middlewares/authMiddleware');
+
+// --- INICIO DE LA CORRECCIÓN ---
+// Importamos AMBAS funciones del validador
 const { 
   validateMovement, 
   handleValidationErrors 
 } = require('../validators/cashboxValidator');
+// --- FIN DE LA CORRECCIÓN ---
 
 
+// Todas estas rutas requieren rol de Admin u Operador
 router.use(protect);
 router.use(adminOrOperator);
 
-// --- CORRECCIÓN DE NOMBRES ---
-router.post('/start', startCashboxSession);
-router.post('/end', endCashboxSession);
-router.get('/session', getActiveCashboxSession);
-router.post('/movement', validateMovement, handleValidationErrors, addCashboxMovement);
-router.get('/summary', getCashboxSummary);
+router.post('/start', startSession);
+router.post('/end', endSession);
+router.get('/session', getSession);
+
+// --- INICIO DE LA CORRECCIÓN ---
+// Añadimos 'handleValidationErrors' después de las reglas de 'validateMovement'
+router.post('/movement', validateMovement, handleValidationErrors, addMovement);
+// --- FIN DE LA CORRECCIÓN ---
+
+router.get('/summary', getSummary);
 
 module.exports = router;
