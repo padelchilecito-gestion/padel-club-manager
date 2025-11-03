1bb73dd25a44288ee1cb490945447351848caf37
padelclilecito-gestion <padelchilecito@gmail.com>
2025-11-03 19:47:27 -0300

    Update products.js

--- server/routes/products.js
+++ server/routes/products.js
@@ -1,20 +1,24 @@
-// server/routes/products.js - CORREGIDO
+// server/routes/products.js (CORREGIDO)
 const express = require('express');
 const router = express.Router();
 const {
-  getAllProducts,    // ✅ Nombre correcto
+  // --- CORRECCIÓN DE NOMBRES ---
+  getAllProducts,
   getProductById,
   createProduct,
   updateProduct,
   deleteProduct,
+  getProductsForShop,
+  getProductForShop,
 } = require('../controllers/productController');
 const { protect, adminOrOperator } = require('../middlewares/authMiddleware');
 
-// Rutas públicas
+// --- CORRECCIÓN DE NOMBRES ---
 router.get('/', getAllProducts);
+router.get('/shop', getProductsForShop);
 router.get('/:id', getProductById);
+router.get('/shop/:id', getProductForShop);
 
-// Rutas protegidas (Admin u Operator)
 router.post('/', protect, adminOrOperator, createProduct);
 router.put('/:id', protect, adminOrOperator, updateProduct);
 router.delete('/:id', protect, adminOrOperator, deleteProduct);
