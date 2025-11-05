5479c3c664bb4a488baf19a139d7c76cad32b4d2
padelclilecito-gestion <padelchilecito@gmail.com>
2025-11-05 20:47:11 -0300

    Update cashbox.js

--- server/routes/cashbox.js
+++ server/routes/cashbox.js
@@ -1,24 +1,29 @@
-// server/routes/cashbox.js (CONSISTENT IMPORT FIX)
+// server/routes/cashbox.js (CORREGIDO)
 const express = require('express');
 const router = express.Router();
-const cashboxController = require('../controllers/cashboxController');
+const {
+  // --- CORRECCIÓN DE NOMBRES (a los originales) ---
+  startSession,
+  endSession,
+  getSession,
+  addMovement,
+  getSummary,
+} = require('../controllers/cashboxController');
 const { protect, adminOrOperator } = require('../middlewares/authMiddleware');
-const { validateMovement } = require('../validators/cashboxValidator');
-const { handleValidationErrors } = require('../middlewares/validationMiddleware');
+const { 
+  validateMovement, 
+  handleValidationErrors 
+} = require('../validators/cashboxValidator');
+
 
 router.use(protect);
 router.use(adminOrOperator);
 
-router.post('/start', cashboxController.startCashboxSession);
-router.post('/close', cashboxController.closeCashboxSession);
-router.get('/current', cashboxController.getActiveCashboxSession);
-router.get('/report', cashboxController.getActiveSessionReport);
-
-router.post(
-  '/movement',
-  validateMovement,
-  handleValidationErrors,
-  cashboxController.addMovement
-);
+// --- CORRECCIÓN DE NOMBRES ---
+router.post('/start', startSession);
+router.post('/end', endSession);
+router.get('/session', getSession);
+router.post('/movement', validateMovement, handleValidationErrors, addMovement);
+router.get('/summary', getSummary);
 
 module.exports = router;
