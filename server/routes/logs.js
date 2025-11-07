387e1944d11e11cdd2d9511226807f91d8a55e70
padelclilecito-gestion <padelchilecito@gmail.com>
2025-11-07 09:43:34 -0300

    Delete server directory

--- server/routes/logs.js
+++
@@ -1,9 +0,0 @@
-// server/routes/logs.js (CORREGIDO Y VERIFICADO)
-const express = require('express');
-const router = express.Router();
-const { getLogs } = require('../controllers/logController');
-const { protect, admin } = require('../middlewares/authMiddleware');
-
-router.get('/', protect, admin, getLogs);
-
-module.exports = router;
