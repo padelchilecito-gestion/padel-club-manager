372c306519b89076d844f4f1c7cbfeaff2c35937
padelclilecito-gestion <padelchilecito@gmail.com>
2025-11-07 16:14:32 -0300

    Update settings.js

--- server/routes/settings.js
+++ server/routes/settings.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const router = express.Router();
-const { getSettings, updateSettings } = require('../controllers/settingController');
+const { 
+  getSettings, 
+  updateSettings,
+  getPublicBusinessHours // --- IMPORTADO ---
+} = require('../controllers/settingController');
 const { protect, admin } = require('../middlewares/authMiddleware');
+
+// --- NUEVA RUTA PÚBLICA ---
+// @route   GET api/settings/business-hours
+// @desc    Get business hours for public booking page
+// @access  Public
+router.get('/business-hours', getPublicBusinessHours);
+// -------------------------
 
 // @route   GET api/settings
 // @desc    Get all settings
