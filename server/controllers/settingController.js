cd1b7b467fd927ec3b7aeeb0b410b28486891a21
padelclilecito-gestion <padelchilecito@gmail.com>
2025-11-07 16:13:56 -0300

    Update settingController.js

--- server/controllers/settingController.js
+++ server/controllers/settingController.js
@@ -1,6 +1,6 @@
 const Setting = require('../models/Setting');
 
-// @desc    Get all settings
+// @desc    Get all settings for Admin
 // @route   GET /api/settings
 // @access  Admin
 const getSettings = async (req, res) => {
@@ -42,7 +42,29 @@ const updateSettings = async (req, res) => {
     }
 };
 
+// --- NUEVA FUNCIÓN ---
+// @desc    Get only the business hours
+// @route   GET /api/settings/business-hours
+// @access  Public
+const getPublicBusinessHours = async (req, res) => {
+    try {
+        const businessHoursSetting = await Setting.findOne({ key: 'BUSINESS_HOURS' });
+        
+        if (businessHoursSetting) {
+            res.json(JSON.parse(businessHoursSetting.value));
+        } else {
+            // Si no hay configuración, se devuelve un objeto vacío (todo cerrado)
+            res.json({});
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Server Error' });
+    }
+};
+// ---------------------
+
 module.exports = {
     getSettings,
     updateSettings,
+    getPublicBusinessHours, // Exportamos la nueva función
 };
