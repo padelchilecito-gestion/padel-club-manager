d4e09fd1662301809e778abd53d79934ebbe368b
padelclilecito-gestion <padelchilecito@gmail.com>
2025-11-10 15:35:18 -0300

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,15 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { AuthProvider } from './contexts/AuthContext'
+import { PublicSettingsProvider } from './contexts/PublicSettingsContext' // --- IMPORTADO ---
 import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <PublicSettingsProvider> {/* --- AÑADIDO --- */}
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PublicSettingsProvider> {/* --- AÑADIDO --- */}
     </AuthProvider>
   </React.StrictMode>,
 )
