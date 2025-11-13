4545cf4a893da0da8cb0006b68125e87f38d226e
padelclilecito-gestion <padelchilecito@gmail.com>
2025-11-13 10:10:06 -0300

    Delete server directory

--- server/config/mercadopago-config.js
+++
@@ -1,6 +0,0 @@
-const { MercadoPagoConfig } = require('mercadopago');
-require('dotenv').config();
-
-const client = new MercadoPagoConfig({ accessToken: process.env.MERCADOPAGO_ACCESS_TOKEN });
-
-module.exports = client;
